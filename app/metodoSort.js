a488cd7aa83b3b2b560466873ead35726b5610a1
William Souza <william.h.souza.com>
2023-04-11 13:24:43 -0300

    v1.0

---
+++ app/metodoSort.js
@@ -0,0 +1,9 @@
+let btnOrdernarPorPreco = document.getElementById('btnOrdenarPorPreco')
+btnOrdernarPorPreco.addEventListener('click', ordenarLivrosPorPreco)
+
+
+function ordenarLivrosPorPreco(){
+    let livrosOrdenados = livros.sort((a, b) => a.preco - b.preco)
+
+    exibirOsLivrosNaTela(livrosOrdenados)
+}
